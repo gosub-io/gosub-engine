9fa2622869f5723d151ecb6e05aa454992008d76
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-10-15 00:11:59 +0300

    feat(css): add basic css parser (rdp) (#134)
    * feat(css): add basic css parser (rdp)
    
    * chore: allow dead code in the ccs3_parser
    
    * fix: resolve all clippy errors/warnnings

---
+++ src/css3_parser/tokens.rs
@@ -0,0 +1,56 @@
+#[derive(PartialEq, Debug, Clone, Copy)]
+pub enum TokenType {
+    Dot,
+    GreaterThan,
+    LessThan,
+    Identifier,
+    OpeningBracket,
+    ClosingBracket,
+    OpeningParenthesis,
+    ClosingParenthesis,
+    Colon,
+    SimiColon,
+    Comma,
+    And,
+    Hash,
+    Media,
+    MediaPrinterType,
+    MediaScreenType,
+    MediaAllType,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub value: String,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, value: String) -> Token {
+        Token { token_type, value }
+    }
+}
+
+pub static TOKENS_REGEXS: [(&str, Option<TokenType>); 19] = [
+    (r"^\.", Some(TokenType::Dot)),
+    (r"^>", Some(TokenType::GreaterThan)),
+    (r"^<", Some(TokenType::LessThan)),
+    (r"^\{", Some(TokenType::OpeningBracket)),
+    (r"^\}", Some(TokenType::ClosingBracket)),
+    (r"^\(", Some(TokenType::OpeningParenthesis)),
+    (r"^\)", Some(TokenType::ClosingParenthesis)),
+    (r"^,", Some(TokenType::Comma)),
+    (r"^:", Some(TokenType::Colon)),
+    (r"^;", Some(TokenType::SimiColon)),
+    (r"^#", Some(TokenType::Hash)),
+    // Media queries tokens
+    (r"^@media", Some(TokenType::Media)),
+    (r"^printer", Some(TokenType::MediaPrinterType)),
+    (r"^screen", Some(TokenType::MediaScreenType)),
+    (r"^all", Some(TokenType::MediaAllType)),
+    (r"^and", Some(TokenType::And)),
+    // General tokens
+    (r"^(\w|-)+", Some(TokenType::Identifier)),
+    (r"^\s+", None),
+    (r"^\/\*[^\/]*\*\/", None),
+];
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ extern crate alloc;
 
 pub mod api;
 #[allow(dead_code)]
+pub mod css3_parser;
+#[allow(dead_code)]
 pub mod html5_parser;
 pub mod testing;
 pub mod types;
