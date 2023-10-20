0061680416493042e038c6486148f1b7efb1d938
Joshua Thijssen <jthijssen@noxlogic.nl>
2023-10-20 13:41:44 +0200

    fixed <li> tests

--- tests/tree_construction.rs
+++ tests/tree_construction.rs
@@ -6,14 +6,12 @@ use test_case::test_case;
 const DISABLED_CASES: &[&str] = &[
     // tests1.dat
     "<!-----><font><div>hello<table>excite!<b>me!<th><i>please!</tr><!--X-->",
-    "<!DOCTYPE html><li>hello<li>world<ul>how<li>do</ul>you</body><!--do-->",
     "<!DOCTYPE html><script> <!-- </script> --> </script> EOF",
     r#"<a href="blah">aba<table><a href="foo">br<tr><td></td></tr>x</table>aoe"#,
     r#"<a href="blah">aba<table><tr><td><a href="foo">br</td></tr>x</table>aoe"#,
     r#"<table><a href="blah">aba<tr><td><a href="foo">br</td></tr>x</table>aoe"#,
     "<table><tr><tr><td><td><span><th><span>X</table>",
     "<ul><li></li><div><li></div><li><li><div><li><address><li><b><em></b><li></ul>",
-    "<ul><li><ul></li><li>a</li></ul></li></ul>",
     // tests2.dat
     "<!DOCTYPE html><dt><div><dd>",
     "<!DOCTYPE html><table><tr>TEST",
