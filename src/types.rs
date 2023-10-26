47077f33bbaea27f1d8626115c44893b41922ea8
Eric Walker <eric.walker@gmail.com>
2023-10-25 19:08:53 -0600

    Add a nom parser for the tree construction tests

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,20 @@
 //! Error results that can be returned from the engine
 use thiserror::Error;
 
-/// Generic error types that can be returned from the library.
+/// Parser error that defines an error (message) on the given position
+#[derive(Debug, PartialEq, Clone)]
+pub struct ParseError {
+    /// Parse error message
+    pub message: String,
+    /// Line number (1-based) of the error
+    pub line: usize,
+    // Column (1-based) on line of the error
+    pub col: usize,
+    // Position (0-based) of the error in the input stream
+    pub offset: usize,
+}
+
+/// Serious errors and errors from third-party libraries
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("ureq error")]
@@ -18,6 +31,9 @@ pub enum Error {
 
     #[error("json parsing error: {0}")]
     JsonSerde(#[from] serde_json::Error),
+
+    #[error("test error: {0}")]
+    Test(String),
 }
 
 /// Result that can be returned which holds either T or an Error
