b902769bb19f6cf55e624c611cdb945d32d75422
Sharktheone <sharktheone@proton.me>
2023-11-21 15:48:23 +0100

    add `bold` parameter to `TextNode::new`

--- src/render_tree/text.rs
+++ src/render_tree/text.rs
@@ -16,40 +16,40 @@ impl TextNode {
         NOTE: I got the default font sizes from https://stackoverflow.com/a/70720104
     */
 
-    fn new(fs: f64) -> Self {
+    fn new(fs: f64, bold: bool) -> Self {
         Self {
             value: "".to_owned(),
             font: DEFAULT_FONT.to_owned(),
             font_size: fs,
-            is_bold: false,
+            is_bold: bold,
         }
     }
 
     pub fn new_heading1() -> Self {
-        TextNode::new(37.)
+        TextNode::new(37., true)
     }
 
     pub fn new_heading2() -> Self {
-        TextNode::new(27.5)
+        TextNode::new(27.5, true)
     }
 
     pub fn new_heading3() -> Self {
-        TextNode::new(21.5)
+        TextNode::new(21.5,true)
     }
 
     pub fn new_heading4() -> Self {
-        TextNode::new(18.5)
+        TextNode::new(18.5,true)
     }
 
     pub fn new_heading5() -> Self {
-        TextNode::new(15.5)
+        TextNode::new(15.5,true)
     }
 
     pub fn new_heading6() -> Self {
-        TextNode::new(12.)
+        TextNode::new(12.,true)
     }
 
     pub fn new_paragraph() -> Self {
-        TextNode::new(18.5)
+        TextNode::new(18.5,false)
     }
 }
