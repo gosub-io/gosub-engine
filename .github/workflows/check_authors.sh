34cda98ba45deb99d5ada335c52b2fdd5a798fb6
Joshua Thijssen <jthijssen@noxlogic.nl>
2023-12-02 10:18:17 +0000

    Update .github/workflows/check_authors.sh
    Co-authored-by: Eric Walker <emwalker@users.noreply.github.com>

--- .github/workflows/check_authors.sh
+++ .github/workflows/check_authors.sh
@@ -12,13 +12,13 @@ is_in_authors() {
 # Only check the first 10 committers found in the PR
 COMMITTERS=$(git log $1 --pretty=format:"%an;%ae" | sort | uniq | head -n 10)
 
-for COMMITTER in "$COMMITTERS" ; do
+for committer in "$COMMITTERS" ; do
   # split sentence in two parts seperated by a ;
-  NAME=$(echo $COMMITTER | cut -d ";" -f 1)
-  EMAIL=$(echo $COMMITTER | cut -d ";" -f 2)
+  local name=$(echo $committer | cut -d ";" -f 1)
+  local email=$(echo $committer | cut -d ";" -f 2)
 
-  if is_in_authors "$EMAIL" == 0 && is_in_authors "$NAME" == 0; then
-    echo "This author is not found in the AUTHORS file"
+  if is_in_authors "$email" == 0 && is_in_authors "$name" == 0; then
+    echo "Author $name <$email> was not found in the AUTHORS file"
     exit 1
   fi
 done
