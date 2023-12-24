9230a161c817ac066c3decd8e7405efcd17af5ae
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-12-24 18:10:40 +0500

    Update error_handler.h

--- errorhandler/error_handler.h
+++ errorhandler/error_handler.h
@@ -4,15 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Custom error codes
+// error codes
 typedef enum {
     ERROR_NONE = 0,
     ERROR_INVALID_INPUT = 1,
     ERROR_FILE_NOT_FOUND = 2,
-    // Add more error codes as needed
+    NOT_FOUND = 404,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    REQUEST_TIMEOUT = 408,
+    INTERNAL_SERVER_ERROR = 500,
+    BAD_GATEWAY = 502,
+    SERVICE_UNAVALIBLE = 503,
+    GATEWAY_TIMEOUT = 504,
 } ErrorCode;
 
 // Function to handle errors
 void handle_error(ErrorCode code, const char* message);
 
 #endif // ERROR_HANDLER_H
+
+//simple error handler header file, needs more working on...
