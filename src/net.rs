15f1d8f80e950dd8d3281d0ec44bd7199f6a4d48
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-02-14 19:46:58 +0100

    Update src/net.rs
    Co-authored-by: Shark <105171995+Sharktheone@users.noreply.github.com>

--- src/net.rs
+++ src/net.rs
@@ -1,6 +1,6 @@
 //! Networking functionality
 //!
-//! This module contains all the networking functionality for the browser. THis is normally the
+//! This module contains all the networking functionality for the browser. This is normally the
 //! lowlevel implementation of the browser. The networking module is responsible for making HTTP
 //! requests, parsing the response and returning the result to the caller.
 //!
