7292f10a45f10817b716da4dc2a1136a5e6ba07d
Shark <sharktheone@proton.me>
2024-02-23 10:07:55 +0100

    use once cell for V8 platform initialisation

--- crates/gosub_webexecutor/src/js/v8.rs
+++ crates/gosub_webexecutor/src/js/v8.rs
@@ -3,6 +3,7 @@ use std::cell::RefCell;
 use std::ops::{Deref, DerefMut};
 use std::rc::Rc;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Once;
 
 pub use array::*;
 pub use compile::*;
@@ -22,8 +23,8 @@ mod object;
 mod value;
 
 // status of the V8 engine
-static PLATFORM_INITIALIZED: AtomicBool = AtomicBool::new(false);
-static PLATFORM_INITIALIZING: AtomicBool = AtomicBool::new(false);
+static V8_INITIALIZING: AtomicBool = AtomicBool::new(false);
+static V8_INITIALIZED: Once = Once::new();
 
 trait FromContext<'a, T> {
     fn from_ctx(ctx: V8Context<'a>, value: T) -> Self;
@@ -44,14 +45,10 @@ const MAX_V8_INIT_SECONDS: u64 = 10;
 
 impl V8Engine<'_> {
     pub fn initialize() {
-        if PLATFORM_INITIALIZED.load(Ordering::SeqCst) {
-            return;
-        }
-
         let mut wait_time = MAX_V8_INIT_SECONDS * 1000;
 
-        if PLATFORM_INITIALIZING.load(Ordering::SeqCst) {
-            while !PLATFORM_INITIALIZED.load(Ordering::SeqCst) {
+        if V8_INITIALIZING.load(Ordering::SeqCst) {
+            while !V8_INITIALIZED.is_completed() {
                 std::thread::sleep(std::time::Duration::from_millis(10));
                 wait_time -= 10;
                 if wait_time <= 9 {
@@ -64,15 +61,15 @@ impl V8Engine<'_> {
             return;
         }
 
-        PLATFORM_INITIALIZING.store(true, Ordering::SeqCst);
 
-        //https://github.com/denoland/rusty_v8/issues/1381
-        let platform = v8::new_unprotected_default_platform(0, false).make_shared();
-        v8::V8::initialize_platform(platform);
-        v8::V8::initialize();
-
-        PLATFORM_INITIALIZED.store(true, Ordering::SeqCst);
-        PLATFORM_INITIALIZING.store(false, Ordering::SeqCst);
+        V8_INITIALIZED.call_once(|| {
+            V8_INITIALIZING.store(true, Ordering::SeqCst);
+            //https://github.com/denoland/rusty_v8/issues/1381
+            let platform = v8::new_unprotected_default_platform(0, false).make_shared();
+            v8::V8::initialize_platform(platform);
+            v8::V8::initialize();
+            V8_INITIALIZING.store(false, Ordering::SeqCst);
+        });
     }
 
     pub fn new() -> Self {
@@ -120,7 +117,7 @@ mod tests {
     use anyhow;
     use colored::Colorize;
 
-    use crate::js::v8::PLATFORM_INITIALIZED;
+    use crate::js::v8::V8_INITIALIZED;
     use crate::js::{JSContext, JSError, JSRuntime, JSValue};
     use crate::Error;
     use crate::Error::JS;
@@ -129,7 +126,7 @@ mod tests {
     fn v8_engine_initialization() {
         let mut engine = crate::js::v8::V8Engine::new();
 
-        assert!(PLATFORM_INITIALIZED.load(Ordering::SeqCst));
+        assert!(V8_INITIALIZED.is_completed());
     }
 
     #[test]
