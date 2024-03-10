7ef81ac05c4b8c7082eadc05408c4ea4c7af3a0e
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-03-10 13:49:12 +0100

    Initial timing

--- crates/gosub_shared/src/timing.rs
+++ crates/gosub_shared/src/timing.rs
@@ -1,152 +1,179 @@
-use core::fmt::{Display, Formatter};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::collections::HashMap;
+use std::sync::Mutex;
+use std::time::Instant;
+use lazy_static::lazy_static;
 
-pub enum Timing {
-    DnsLookup,
-    TcpConnection,
-    TlsHandshake,
-    ServerProcessing,
-    ContentTransfer,
-    HtmlParse,
-    CssParse,
-    JsParse,
-    RenderTree,
+type TimerId = uuid::Uuid;
+
+fn new_timer_id() -> TimerId {
+    uuid::Uuid::new_v4()
 }
 
-#[derive(Default, Debug, Clone)]
-pub struct Timer {
-    start: u64,
-    end: u64,
-}
-
-impl Timer {
-    pub fn new() -> Timer {
-        Timer { start: 0, end: 0 }
-    }
-
-    pub fn start(&mut self) {
-        let current_time = SystemTime::now();
-        let duration_since_epoch = current_time
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards");
-        let milliseconds_since_epoch =
-            duration_since_epoch.as_secs() * 1000 + u64::from(duration_since_epoch.subsec_millis());
-
-        self.start = milliseconds_since_epoch;
-    }
-
-    pub fn end(&mut self) {
-        let current_time = SystemTime::now();
-        let duration_since_epoch = current_time
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards");
-        let milliseconds_since_epoch =
-            duration_since_epoch.as_secs() * 1000 + u64::from(duration_since_epoch.subsec_millis());
-
-        self.end = milliseconds_since_epoch;
-    }
-
-    pub fn duration(&self) -> u64 {
-        self.end - self.start
-    }
-}
-
-/// Timing information for a single request. Timing is measured in milliseconds.
 #[derive(Default, Debug, Clone)]
 pub struct TimingTable {
-    /// Time spent making DNS queries.
-    pub dns_lookup: Timer,
-    /// Time spent establishing TCP connection.
-    pub tcp_connection: Timer,
-    /// Time spent performing TLS handshake.
-    pub tls_handshake: Timer,
-    /// Time spent sending HTTP request.
-    pub server_processing: Timer,
-    /// Time spent waiting for HTTP response.
-    pub content_transfer: Timer,
-    /// Time spent parsing HTML.
-    pub html_parse: Timer,
-    /// Time spent parsing CSS.
-    pub css_parse: Timer,
-    /// Time spent parsing JS.
-    pub js_parse: Timer,
-    /// Time spent generating render_tree
-    pub render_tree: Timer,
+    timers: HashMap<TimerId, Timer>,
+    namespaces: HashMap<String, Vec<TimerId>>,
 }
 
-impl Display for TimingTable {
-    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        writeln!(f, "DNS lookup:        {:6}ms", self.dns_lookup.duration())?;
-        writeln!(
-            f,
-            "TCP connection:    {:6}ms",
-            self.tcp_connection.duration()
-        )?;
-        writeln!(
-            f,
-            "TLS handshake:     {:6}ms",
-            self.tls_handshake.duration()
-        )?;
-        writeln!(
-            f,
-            "Server processing: {:6}ms",
-            self.server_processing.duration()
-        )?;
-        writeln!(
-            f,
-            "Content transfer:  {:6}ms",
-            self.content_transfer.duration()
-        )?;
-        writeln!(f, "HTML parse:        {:6}ms", self.html_parse.duration())?;
-        writeln!(f, "CSS parse:         {:6}ms", self.css_parse.duration())?;
-        writeln!(f, "JS parse:          {:6}ms", self.js_parse.duration())?;
-        writeln!(f, "Render tree:       {:6}ms", self.render_tree.duration())?;
 
-        Ok(())
-    }
+pub struct Stats {
+    count: u64,
+    total: u64,
+    min: u64,
+    max: u64,
+    avg: u64,
+    p50: u64,
+    p75: u64,
+    p95: u64,
+    p99: u64,
+}
+
+fn percentage_to_index(count: u64, percentage: f64) -> usize {
+    (count as f64 * percentage) as usize
 }
 
 impl TimingTable {
-    /// Starts a timing table, setting the start time to the current time.
-    pub fn start(&mut self, timing: Timing) {
-        match timing {
-            Timing::DnsLookup => self.dns_lookup.start(),
-            Timing::TcpConnection => self.tcp_connection.start(),
-            Timing::TlsHandshake => self.tls_handshake.start(),
-            Timing::ServerProcessing => self.server_processing.start(),
-            Timing::ContentTransfer => self.content_transfer.start(),
-            Timing::HtmlParse => self.html_parse.start(),
-            Timing::CssParse => self.css_parse.start(),
-            Timing::JsParse => self.js_parse.start(),
-            Timing::RenderTree => self.render_tree.start(),
+    pub fn new() -> TimingTable {
+        TimingTable {
+            timers: HashMap::new(),
+            namespaces: HashMap::new(),
         }
     }
 
-    pub fn end(&mut self, timing: Timing) {
-        match timing {
-            Timing::DnsLookup => self.dns_lookup.end(),
-            Timing::TcpConnection => self.tcp_connection.end(),
-            Timing::TlsHandshake => self.tls_handshake.end(),
-            Timing::ServerProcessing => self.server_processing.end(),
-            Timing::ContentTransfer => self.content_transfer.end(),
-            Timing::HtmlParse => self.html_parse.end(),
-            Timing::CssParse => self.css_parse.end(),
-            Timing::JsParse => self.js_parse.end(),
-            Timing::RenderTree => self.render_tree.end(),
+    pub fn start_timer(&mut self, namespace: &str, context: Option<String>) -> TimerId {
+        let timer = Timer::new(context);
+        self.timers.insert(timer.id, timer.clone());
+        self.namespaces.entry(namespace.to_string()).or_insert(Vec::new()).push(timer.id);
+
+        timer.id
+    }
+
+    pub fn stop_timer(&mut self, timer_id: TimerId) {
+        if let Some(timer) = self.timers.get_mut(&timer_id) {
+            timer.end();
         }
     }
 
-    pub fn duration(&self, timing: Timing) -> u64 {
-        match timing {
-            Timing::DnsLookup => self.dns_lookup.duration(),
-            Timing::TcpConnection => self.tcp_connection.duration(),
-            Timing::TlsHandshake => self.tls_handshake.duration(),
-            Timing::ServerProcessing => self.server_processing.duration(),
-            Timing::ContentTransfer => self.content_transfer.duration(),
-            Timing::HtmlParse => self.html_parse.duration(),
-            Timing::CssParse => self.css_parse.duration(),
-            Timing::JsParse => self.js_parse.duration(),
-            Timing::RenderTree => self.render_tree.duration(),
+    pub fn get_stats(&self, timers: &Vec<TimerId>) -> Stats {
+        let mut durations: Vec<u64> = Vec::new();
+        for timer_id in timers {
+            if let Some(timer) = self.timers.get(timer_id) {
+                if timer.has_finished() {
+                    durations.push(timer.duration_ms);
+                }
+            }
+        }
+
+        durations.sort();
+        let count = durations.len() as u64;
+        let total = durations.iter().sum();
+        let min = durations.first().unwrap_or(&0).clone();
+        let max = durations.last().unwrap_or(&0).clone();
+        let avg = total / count;
+        let p50 = durations[percentage_to_index(count, 0.50)];
+        let p75 = durations[percentage_to_index(count, 0.75)];
+        let p95 = durations[percentage_to_index(count, 0.95)];
+        let p99 = durations[percentage_to_index(count, 0.99)];
+
+        Stats {
+            count,
+            total,
+            min,
+            max,
+            avg,
+            p50,
+            p75,
+            p95,
+            p99,
+        }
+    }
+
+    pub fn print_timings(&self, show_details: bool) {
+        println!("Namespace            |    Count |      Total |        Min |        Max |        Avg |        50% |        75% |        95% |        99%");
+        println!("----------------------------------------------------------------------------------------------------------------------------------------");
+        for (namespace, timers) in &self.namespaces {
+            let stats = self.get_stats(timers);
+            println!("{:20} | {:8} | {:8}ms | {:8}ms | {:8}ms | {:8}ms | {:8}ms | {:8}ms | {:8}ms | {:8}ms", namespace, stats.count, stats.total, stats.min, stats.max, stats.avg, stats.p50, stats.p75, stats.p95, stats.p99);
+
+            if show_details {
+                for timer_id in timers {
+                    let timer = self.timers.get(timer_id).unwrap();
+                    if timer.has_finished() {
+                        println!("  {:18} | {:8} | {:8}ms", timer.context.clone().unwrap_or("".into()), 1, timer.duration_ms);
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn duration(&self, timer_id: TimerId) -> u64 {
+        if let Some(timer) = self.timers.get(&timer_id) {
+            timer.duration()
+        } else {
+            0
+        }
+    }
+}
+
+lazy_static! {
+    pub static ref TIMING_TABLE: Mutex<TimingTable> = Mutex::new(TimingTable::default());
+}
+
+macro_rules! timing_start {
+    ($namespace:expr, $context:expr) => {{
+        TIMING_TABLE.lock().unwrap().start_timer($namespace, Some($context.to_string()))
+    }};
+
+    ($namespace:expr) => {{
+        TIMING_TABLE.lock().unwrap().start_timer($namespace, None)
+    }};
+}
+
+macro_rules! timing_stop {
+    ($timer_id:expr) => {{
+        TIMING_TABLE.lock().unwrap().stop_timer($timer_id);
+    }};
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Timer {
+    id: TimerId,
+    context: Option<String>,
+    start: Instant,
+    end: Option<Instant>,
+    duration_ms: u64,
+}
+
+impl Timer {
+    pub fn new(context: Option<String>) -> Timer {
+        Timer {
+            id: new_timer_id(),
+            context,
+            start: Instant::now(),
+            end: None,
+            duration_ms: 0
+        }
+    }
+
+    pub fn start(&mut self) {
+        self.start = Instant::now();
+    }
+
+    pub fn end(&mut self) {
+        self.end = Some(Instant::now());
+        self.duration_ms = self.end.expect("").duration_since(self.start).as_millis() as u64;
+    }
+
+    pub(crate) fn has_finished(&self) -> bool {
+        return self.end.is_some();
+    }
+
+    pub fn duration(&self) -> u64 {
+        if let Some(end) = self.end {
+            end.duration_since(self.start).as_millis() as u64
+        } else {
+            0
         }
     }
 }
@@ -155,30 +182,39 @@ impl TimingTable {
 mod tests {
     use super::*;
     use std::thread::sleep;
+    use rand::random;
 
     #[test]
     fn test_timing_defaults() {
-        let timing_table = TimingTable::default();
 
-        assert_eq!(0, timing_table.duration(Timing::DnsLookup));
-        assert_eq!(0, timing_table.duration(Timing::TcpConnection));
-        assert_eq!(0, timing_table.duration(Timing::RenderTree));
-    }
-
-    #[test]
-    fn test_timings() {
-        let mut timingtable = TimingTable::default();
-
-        timingtable.start(Timing::DnsLookup);
-        timingtable.start(Timing::TcpConnection);
-        sleep(std::time::Duration::from_millis(150));
-        timingtable.end(Timing::DnsLookup);
+        let t = timing_start!("dns.lookup", "www.foo.bar");
         sleep(std::time::Duration::from_millis(10));
-        timingtable.end(Timing::TcpConnection);
+        timing_stop!(t);
 
-        assert!(timingtable.duration(Timing::DnsLookup) > 100);
-        assert!(
-            timingtable.duration(Timing::TcpConnection) > timingtable.duration(Timing::DnsLookup)
-        );
+        for i in 0..50 {
+            let t = timing_start!("html5.parse", "index.html");
+            sleep(std::time::Duration::from_millis(
+                random::<u64>() % 50
+            ));
+            timing_stop!(t);
+        }
+
+        let t = timing_start!("html5.parse", "index.html");
+        sleep(std::time::Duration::from_millis(20));
+        timing_stop!(t);
+
+        let t = timing_start!("html5.parse", "page2.html");
+        sleep(std::time::Duration::from_millis(20));
+        timing_stop!(t);
+
+        let t = timing_start!("html5.parse", "page3.html");
+        sleep(std::time::Duration::from_millis(20));
+        timing_stop!(t);
+
+        let t = timing_start!("css.parse");
+        sleep(std::time::Duration::from_millis(20));
+        timing_stop!(t);
+
+        TIMING_TABLE.lock().unwrap().print_timings(true);
     }
 }
