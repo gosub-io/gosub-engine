baf4e6f47a66a5fe38490d71e16d5e0c84acc479
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-07-21 17:12:36 +0200

    Reverted back the FIXTURE_ROOT

--- crates/gosub_testing/src/testing.rs
+++ crates/gosub_testing/src/testing.rs
@@ -2,5 +2,5 @@
 pub mod tokenizer;
 pub mod tree_construction;
 
-pub const FIXTURE_ROOT: &str = "./crates/gosub_html5/tests/data/html5lib-tests";
+pub const FIXTURE_ROOT: &str = "./tests/data/html5lib-tests";
 pub const TREE_CONSTRUCTION_PATH: &str = "tree-construction";
