1001faca39d4070bd1e074972b28884edf795161
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-12-19 16:43:30 +0100

    Added prelude for connecting to external applications

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+pub mod prelude;
+
 mod engine;
 
 #[cfg(target_arch = "wasm32")]
 mod wasm;
+
---
+++ src/prelude.rs
@@ -0,0 +1,15 @@
+pub use gosub_shared::traits::config::*;
+
+pub use gosub_css3::system::Css3System;
+
+pub use gosub_html5::document::builder::DocumentBuilderImpl;
+pub use gosub_html5::document::document_impl::DocumentImpl;
+pub use gosub_html5::document::fragment::DocumentFragmentImpl;
+pub use gosub_html5::parser::Html5Parser;
+
+pub use gosub_taffy::TaffyLayouter;
+pub use gosub_renderer::draw::TreeDrawerImpl;
+pub use gosub_rendering::render_tree::RenderTree;
+
+pub use gosub_cairo::CairoBackend;
+pub use gosub_vello::VelloBackend;
