a68c777c431123cf4ab71f0838c3f9286c0e4fc1
Joshua Thijssen <jthijssen@noxlogic.nl>
2026-04-27 18:38:44 +0200

    ci: fix check_authors.sh shell bugs and bump github-script to v7

--- .github/workflows/check_authors.sh
+++ .github/workflows/check_authors.sh
@@ -2,29 +2,25 @@
 
 is_in_authors() {
   local target=$1
-  if grep -Fq "$target" AUTHORS; then
-    return 1
-  else
-    return 0
-  fi
+  grep -Fq "$target" AUTHORS
 }
 
 # Get all commits in this PR
-COMMITTERS=$(git log -n $1 --pretty=format:"%an;%ae" | sort | uniq | grep -v noreply.github.com)
+COMMITTERS=$(git log -n "$1" --pretty=format:"%an;%ae" | sort | uniq | grep -v noreply.github.com)
 
-# shellcheck disable=SC2066
-for committer in "$COMMITTERS" ; do
+while IFS= read -r committer; do
+  [ -z "$committer" ] && continue
+
   echo "Validating author: $committer"
 
-  # split sentence in two parts seperated by a ;
-  name=$(echo $committer | cut -d ";" -f 1)
-  email=$(echo $committer | cut -d ";" -f 2)
+  name=$(echo "$committer" | cut -d ";" -f 1)
+  email=$(echo "$committer" | cut -d ";" -f 2)
 
-  if is_in_authors "$email" == 0 && is_in_authors "$name" == 0; then
+  if ! is_in_authors "$email" && ! is_in_authors "$name"; then
     echo "Author $name <$email> was not found in the AUTHORS file"
     exit 1
   fi
-done
+done <<< "$COMMITTERS"
 
 echo "All authors are found in the AUTHORS file."
 exit 0
