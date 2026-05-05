6b5b610eb06b9ac6af2b1a21b08b63d09232f7e0
Joshua Thijssen <jthijssen@noxlogic.nl>
2026-05-05 18:39:01 +0200

    Using the media ID tthrough the media store before using pixel data

--- crates/gosub_pipeline/src/rasterizer/cairo/brush.rs
+++ crates/gosub_pipeline/src/rasterizer/cairo/brush.rs
@@ -1,56 +1,46 @@
-use crate::common::geo::Rect;
-use crate::common::get_media_store;
-use crate::common::media::{Media, MediaType};
-use crate::painter::commands::brush::Brush;
 use gtk4::cairo::Context;
 use gtk4::gdk_pixbuf::{Colorspace, Pixbuf};
 use gtk4::glib::Bytes;
 use gtk4::prelude::GdkCairoContextExt;
+use crate::common::geo::Rect;
+use crate::common::get_media_store;
+use crate::painter::commands::brush::Brush;
 
+// Sets the given brush to the context. In case of an image brush, rect defines the scale size of the image.
 pub fn set_brush(cr: &Context, brush: &Brush, rect: Rect) {
     match brush {
         Brush::Solid(color) => {
             cr.set_source_rgba(color.r() as f64, color.g() as f64, color.b() as f64, color.a() as f64);
         }
         Brush::Image(media_id) => {
+            // If the rect has no width or height, we do not need to draw the image. So we can leave the brush as-is.
             if rect.width == 0.0 || rect.height == 0.0 {
                 return;
             }
 
-            let store = get_media_store();
-            let binding = store.read().unwrap();
-            let media = binding.get(*media_id, MediaType::Image);
-            let Media::Image(img) = &*media else {
+            let binding = get_media_store();
+            let Ok(media_store) = binding.read() else {
+                log::warn!("Failed to acquire media store lock, skipping image brush");
                 return;
             };
+            let media = media_store.get_image(*media_id);
+            let img = &media.image;
 
-            let bytes = Bytes::from(img.image.as_raw().as_slice());
-            let pixbuf = Pixbuf::from_bytes(
-                &bytes,
-                Colorspace::Rgb,
-                true,
-                8,
-                img.image.width() as i32,
-                img.image.height() as i32,
-                img.image.width() as i32 * 4,
-            );
+            let bytes = Bytes::from(img.as_raw().as_slice());
+            let pixbuf = Pixbuf::from_bytes(&bytes, Colorspace::Rgb, true, 8, img.width() as i32, img.height() as i32, img.width() as i32 * 4);
 
-            let scale_x = rect.width / img.image.width() as f64;
-            let scale_y = rect.height / img.image.height() as f64;
+            let scale_x = rect.width / img.width() as f64;
+            let scale_y = rect.height / img.height() as f64;
 
+            // Create a scaled version of the image. This does not really sound like a good idea, but i have to find better ways to deal
+            // with scaled images.
             let scaled_pixbuf = Pixbuf::new(Colorspace::Rgb, true, 8, rect.width as i32, rect.height as i32).unwrap();
-            pixbuf.scale(
-                &scaled_pixbuf,
-                0,
-                0,
-                rect.width as i32,
-                rect.height as i32,
-                0.0,
-                0.0,
-                scale_x,
-                scale_y,
-                gtk4::gdk_pixbuf::InterpType::Bilinear,
-            );
+            pixbuf.scale(&scaled_pixbuf,
+                0,0,
+                rect.width as i32, rect.height as i32,
+                0.0, 0.0,
+                scale_x, scale_y,
+                gtk4::gdk_pixbuf::InterpType::Bilinear);
 
             cr.set_source_pixbuf(&scaled_pixbuf, rect.x, rect.y);
         }
