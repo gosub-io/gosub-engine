b51e9d9d136fe7a4a865537b5cb075231cba99eb
Joshua Thijssen <jthijssen@noxlogic.nl>
2026-06-02 09:56:42 +0200

    implemented parley font system

--- crates/gosub_fontmanager/src/lib.rs
+++ crates/gosub_fontmanager/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod flatland;
 pub mod font_manager;
+pub mod parley_system;
 
 pub use font_manager::font_info::FontInfo;
 pub use font_manager::manager::FontManager;
+pub use parley_system::ParleyFontSystem;
