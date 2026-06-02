94b5e85aabbf81c19753e05c6eb7f9ef80d3ba49
Joshua Thijssen <jthijssen@noxlogic.nl>
2026-06-02 10:47:29 +0200

    passing font context

--- crates/gosub_render_pipeline/src/layouter/text/parley.rs
+++ crates/gosub_render_pipeline/src/layouter/text/parley.rs
@@ -1,12 +1,14 @@
 use crate::common::font::parley::get_parley_layout;
 use crate::common::font::FontInfo;
 use crate::common::geo::Dimension;
+use parley::FontContext;
 
 pub fn get_text_layout(
     text: &str,
     font_info: &FontInfo,
     max_width: f64,
     _dpi_scale_factor: f32,
+    font_cx: &mut FontContext,
 ) -> Result<Dimension, anyhow::Error> {
     let layout = get_parley_layout(
         text,
@@ -16,6 +18,7 @@ pub fn get_text_layout(
         font_info.weight,
         max_width,
         font_info.alignment.clone(),
+        font_cx,
     );
 
     Ok(Dimension {
